eb09ebddc313c4b06faa5998472ad6e2fcac4bf6
andre.lermen <andre.lermen@MVBRPFBDSK5181.mvpfb.local>
2014-02-19 11:47:15 -0300

    aplicação com jsf e jpa

---
+++ InitJSFJPA/src/main/java/br/com/web/ModuloBean.java
@@ -0,0 +1,43 @@
+package br.com.web;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.admin.Modulo;
+import br.com.admin.ModuloService;
+
+//@RequestScoped
+//@ManagedBean
+@Named
+public class ModuloBean {
+	
+	private ModuloService moduloService;
+	
+	private List<Modulo> modulos;
+	
+	@Inject
+	public ModuloBean(ModuloService moduloService){
+		this.moduloService = moduloService;		
+	}
+	
+	public ModuloBean(){
+	}
+	
+	@PostConstruct
+	private void init(){
+		modulos = this.moduloService.listarAtivos();
+	}
+
+	public List<Modulo> getModulos() {
+		return modulos;
+	}
+
+	public void setModulos(List<Modulo> modulos) {
+		this.modulos = modulos;
+	}
+
+
+}
